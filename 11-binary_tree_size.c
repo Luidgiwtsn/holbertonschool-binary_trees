25875be390dc5495b1839a453167a95663737eb4
luidgiwtsn <lwbordeaux@gmail.com>
2025-08-05 14:15:46 +0200

    Add 11-binary_tree_size.c|Add 11-main.c|edit binary_trees.h

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - mesure la taille (nombre de noeuds) d'une arbre binaire
+ * @tree: pointeur vers la racine de l'arbre
+ * return: taille de l'arbre, ou 0 si tree est NULL
+ */
+
+ size_t binary_tree_size(const binary_tree_t *tree)
+ {
+    if (tree == NULL)
+    {
+        return (0);
+    }    
+        return (1+ binary_tree_size(tree->left) + binary_tree_size(tree->right));
+
+ }
